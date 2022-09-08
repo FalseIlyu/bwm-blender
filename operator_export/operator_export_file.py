Correct rotation for bone export, entities export

operator_export/operator_export_file.py
@@ -14,6 +14,7 @@ from ..operator_utilities.file_definition_bwm import (
     BWMFile,
     Bone,
     CollisionPoint,
+    Entity,
     FileType,
 )
 from .operator_export_mesh import organise_mesh_data
@@ -39,21 +40,35 @@ def organize_bwm_data(settings, collection: bpy.types.Collection) -> BWMFile:
         file.modelHeader.type = FileType.SKIN
 
         bone_collection = collection.children.get("bones")
-        for obj in bone_collection.objects.values():
-            if obj.type == "EMPTY":
-                index = int(obj.name)
-                transformation_matrix = np.transpose(obj.matrix_world)
-                transformation_matrix = xyz_to_zxy(transformation_matrix)
-                bone = Bone()
-                bone.xaxis = transformation_matrix[0][:3]
-                bone.yaxis = transformation_matrix[1][:3]
-                bone.zaxis = transformation_matrix[2][:3]
-                bone.position = transformation_matrix[3][:3]
-                file.bones.insert(index, bone)
-        file.modelHeader.boneCount = len(file.bones)
+        if bone_collection:
+            for obj in bone_collection.objects.values():
+                if obj.type == "EMPTY":
+                    index = int(obj.name)
+                    rotation = xyz_to_zxy(np.array(obj.matrix_world)[:3, :3])
+                    bone = Bone()
+                    bone.zaxis = rotation[0]
+                    bone.yaxis = rotation[2]
+                    bone.xaxis = rotation[1]
+                    bone.position = xyz_to_zxy(obj.location)
+                    file.bones.insert(index, bone)
+            file.modelHeader.boneCount = len(file.bones)
 
     if settings["type"] == "OPT_MODEL" or settings["experimental"]:
         file.modelHeader.type = FileType.MODEL
+
+        entity_collection = collection.children.get("entities")
+        if entity_collection:
+            for obj in entity_collection.objects.values():
+                if obj.type == "EMPTY":
+                    rotation = xyz_to_zxy(np.array(obj.matrix_world)[:3, :3])
+                    entity = Entity()
+                    entity.zaxis = rotation[0]
+                    entity.yaxis = rotation[2]
+                    entity.xaxis = rotation[1]
+                    entity.position = xyz_to_zxy(obj.location)
+                    entity.name = obj.name.split(".")[0]
+                    file.entities.append(entity)
+            file.modelHeader.entityCount = len(file.entities)
 
         collision = collection.children.get("collision")
         if collision:

operator_export/operator_export_mesh.py
@@ -8,6 +8,7 @@ from typing import Dict, List, Tuple
 import math
 from statistics import mean
 
+import numpy as np
 import bpy
 import bmesh
 
@@ -117,11 +118,12 @@ def create_basic_description(
     if lod > 1:
         mesh_desc.unknown_int = 1
     mesh_desc.name = obj.name.split(".")[0]
-    rotation = xyz_to_zxy([vector[:3] for vector in obj.matrix_world[:3]])
-    mesh_desc.zaxis = rotation[0]
+    rotation = np.transpose(obj.matrix_world)
+    rotation = xyz_to_zxy(rotation[:3, :3])
+    mesh_desc.zaxis = rotation[2]
     mesh_desc.yaxis = rotation[1]
-    mesh_desc.xaxis = rotation[2]
-    mesh_desc.position = obj.location
+    mesh_desc.xaxis = rotation[0]
+    mesh_desc.position = xyz_to_zxy(obj.location)
 
     # Organise mesh description metadata
     obj = obj.to_mesh(preserve_all_data_layers=True)
@@ -221,13 +223,13 @@ def create_bounds(
     """
     num_vertex = len(vertices)
     mesh_desc.vertexSize = num_vertex
-    loc_list = [tuple(xyz_to_zxy(vertex.co)) for vertex in vertices]
-    x = [loc[0] for loc in loc_list]
-    y = [loc[1] for loc in loc_list]
-    z = [loc[2] for loc in loc_list]
-    mesh_desc.cent = (mean(x), mean(y), mean(z))
-    mesh_desc.box1 = (min(x), min(y), min(z))
-    mesh_desc.box2 = (max(x), max(y), max(z))
-    mesh_desc.radius = max([math.dist(loc, mesh_desc.cent) for loc in loc_list])
+    list_loc = [tuple(xyz_to_zxy(vertex.co)) for vertex in vertices]
+    list_x = [loc[0] for loc in list_loc]
+    list_y = [loc[1] for loc in list_loc]
+    list_z = [loc[2] for loc in list_loc]
+    mesh_desc.cent = (mean(list_x), mean(list_y), mean(list_z))
+    mesh_desc.box1 = (min(list_x), min(list_y), min(list_z))
+    mesh_desc.box2 = (max(list_x), max(list_y), max(list_z))
+    mesh_desc.radius = max([math.dist(loc, mesh_desc.cent) for loc in list_loc])
     mesh_desc.unknowns1 = mesh_desc.box2
     mesh_desc.height = mesh_desc.box2[1]

operator_utilities/vector_utils.py
@@ -18,22 +18,24 @@ def zxy_to_xyz(matrix_or_vector: np.ndarray) -> np.ndarray:
     coordinate to an xyz one.
     """
     if len(matrix_or_vector) > 3:
-        return np.array(
+        return np.matmul(
             [
                 [0.0, 0.0, 1.0, 0.0],  # Z -> X
                 [1.0, 0.0, 0.0, 0.0],  # X -> Y
                 [0.0, 1.0, 0.0, 0.0],  # Y -> Z
                 [0.0, 0.0, 0.0, 1.0],  # Pos
-            ]
-        ).dot(matrix_or_vector)
+            ],
+            matrix_or_vector,
+        )
 
-    return np.array(
+    return np.matmul(
         [
             [0.0, 0.0, 1.0],  # Z -> X
             [1.0, 0.0, 0.0],  # X -> Y
             [0.0, 1.0, 0.0],  # Y -> Z
-        ]
-    ).dot(matrix_or_vector)
+        ],
+        matrix_or_vector,
+    )
 
 
 def xyz_to_zxy(matrix_or_vector: np.ndarray) -> np.ndarray:
@@ -42,24 +44,24 @@ def xyz_to_zxy(matrix_or_vector: np.ndarray) -> np.ndarray:
     coordinate to an zxy one.
     """
     if len(matrix_or_vector) > 3:
-        return list(
-            np.array(
-                [
-                    [0.0, 1.0, 0.0, 0.0],  # X -> Z
-                    [0.0, 0.0, 1.0, 0.0],  # Y -> Y
-                    [1.0, 0.0, 0.0, 0.0],  # Z -> Z
-                    [0.0, 0.0, 0.0, 1.0],  # Pos
-                ]
-            ).dot(matrix_or_vector)
+        return np.matmul(
+            [
+                [0.0, 1.0, 0.0, 0.0],  # X -> Z
+                [0.0, 0.0, 1.0, 0.0],  # Y -> X
+                [1.0, 0.0, 0.0, 0.0],  # Z -> Y
+                [0.0, 0.0, 0.0, 1.0],  # Pos
+            ],
+            matrix_or_vector,
         )
 
-    return np.array(
+    return np.matmul(
         [
             [0.0, 1.0, 0.0],  # X -> Z
-            [0.0, 0.0, 1.0],  # Y -> Y
-            [1.0, 0.0, 0.0],  # Z -> Z
-        ]
-    ).dot(matrix_or_vector)
+            [0.0, 0.0, 1.0],  # Y -> X
+            [1.0, 0.0, 0.0],  # Z -> Y
+        ],
+        matrix_or_vector,
+    )
 
 
 def construct_transformation_matrix(